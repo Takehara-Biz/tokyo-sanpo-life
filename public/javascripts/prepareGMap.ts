61cc4a172db181040b7996df9eb395115927c545
Liberty Takehara <libertytakehara@Libertys-MacBook-Air.local>
2023-12-10 20:17:11 +0900

    update

--- public/javascripts/prepareGMap.ts
+++ public/javascripts/prepareGMap.ts
@@ -26,7 +26,7 @@ function createInfoContent(sanpoContent: SanpoContent): string {
 }
 
 async function initMap(): Promise<void> {
-  // The location of Uluru
+  // The location of Tokyo Station
   let position = { lat: 35.6812405, lng: 139.7645499 };
 
   // Request needed libraries.
@@ -34,28 +34,85 @@ async function initMap(): Promise<void> {
   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 
-  // The map, centered at Uluru
+  let center;
+  let peripheral;
+
   map = new Map(
     document.getElementById('map') as HTMLElement,
     {
-      zoom: 13,
+      
       center: position,
       mapId: 'DEMO_MAP_ID',
+      minZoom: 9,
+      panControl: true,
+      streetViewControl: false,
+      fullscreenControl: false,
+      zoom: 13,
     }
   );
 
-  let infoWindows: google.maps.InfoWindow[] = [];
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (pos: GeolocationPosition) => {
+        position = {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        };
+
+        map.setCenter(position);
+
+        //縁の薄い青丸
+        peripheral = new google.maps.Circle({
+          strokeColor: '#115EC3',
+          strokeOpacity: 0.2,
+          strokeWeight: 1,
+          fillColor: '#115EC3',
+          fillOpacity: 0.2,
+          map: map,
+          center: position,
+          radius: 100
+      });        
+      //  中央の濃い青丸
+      center = new google.maps.Marker({
+          position: position,
+          map: map,
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            fillColor: '#115EC3',
+            fillOpacity: 1,
+            strokeColor: 'white',
+            strokeWeight: 2,
+            scale: 7
+          }, 
+      });
+      },
+      () => {
+        console.warn('Failed getting the current location!');
+      }
+    );
+  } else {
+    console.warn("Browser doesn't support Geolocation!");
+  }
+
+
+// const locationButton = document.createElement("button");
+//   locationButton.textContent = "現在地";
+const locationButton = document.createElement("button");
+const spanElement = document.createElement("span");
+spanElement.innerHTML = 'my_location';
+spanElement.classList.add('material-symbols-outlined');
+spanElement.style.cssText = 'color: #666666;'
+locationButton.appendChild(spanElement);
+locationButton.style.cssText = "background-color: white; padding: 8px; border: 0px solid; border-radius: 2px; margin-right: 10px; filter: drop-shadow(0px 0px 1px rgba(0,0,0,0.2));";
+  //locationButton.style.cssText = "background-color: red; font-size: 20px;"
+  //locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(locationButton);
+
   let markers: google.maps.marker.AdvancedMarkerElement[] = [];
 
   DummyData.map((value: SanpoContent) => {
     
     let contentString = createInfoContent(value);
-
-    let infoWindow = new google.maps.InfoWindow({
-      content: contentString,
-      ariaLabel: "Uluru",
-      minWidth: 1000,
-    });
 
     position = { lat: value.lat, lng: value.lon };
 
@@ -67,22 +124,12 @@ async function initMap(): Promise<void> {
     });
 
     marker.addListener('click', function () {
-      // infoWindow.open({
-      //   anchor: marker,
-      //   map: map,
-      // });
-      const map2 = document.getElementById('map');
-      //map2!.style.height = 'calc(100% - 66px - 48px - 200px)';
       const info = document.getElementById('info');
       info!.style.display = 'block';
       let contentString = createInfoContent(value);
       info!.innerHTML = contentString;
     });
     map.addListener('click', function () {
-      // infoWindow.open({
-      //   anchor: marker,
-      //   map: map,
-      // });
       const info = document.getElementById('info');
       info!.innerHTML = '';
       info!.style.display = 'none';
@@ -91,7 +138,6 @@ async function initMap(): Promise<void> {
     });
 
     markers.push(marker);
-    infoWindows.push(infoWindow);
   });
 }
 
