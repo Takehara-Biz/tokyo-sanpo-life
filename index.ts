e321f98ee0f7fb751cc994c223b600db15b517d0
Liberty Takehara <libertytakehara@Libertys-MacBook-Air.local>
2023-12-10 17:43:18 +0900

    update

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
 import express from 'express'
+import favicon from 'serve-favicon'
 const app: express.Express = express()
 const port = 3000
 
 app.set('view engine', 'ejs');
 //app.use(express.static(__dirname + '/public'));
 app.use(express.static('public'));
+app.use(favicon('./public/images/favicon.ico'));
 
 app.get('/how-to-use', function(req, res, next) {
-  res.render('pages/how-to-use', { title: 'How to Use' });
+  res.render('pages/how-to-use', { menu1: true });
 });
 app.get('/column', function(req, res, next) {
-  res.render('pages/column', { title: 'Column' });
+  res.render('pages/column', { menu2: true });
 });
 app.get('/map', function(req, res, next) {
   res.render('pages/map', { title: 'Map' });
