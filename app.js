98267c40166c09d22318ab7b694d761ba7e5ae25
Liberty Takehara <libertytakehara@Libertys-MacBook-Air.local>
2023-12-28 23:39:15 +0900

    login test

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// [BEGIN] Express setting
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -39,3 +41,31 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// [  END] Express setting
+
+// [BEGIN] Firebase setting
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAW0fJXkv5YGBZDcFdNeaQdH4s0JwRVIec",
+  authDomain: "tokyo-sanpo-life.firebaseapp.com",
+  projectId: "tokyo-sanpo-life",
+  storageBucket: "tokyo-sanpo-life.appspot.com",
+  messagingSenderId: "702612803712",
+  appId: "1:702612803712:web:1700ffd54eef1da20a1665",
+  measurementId: "G-DNVGJXLX5J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// [  END] Firebase setting
