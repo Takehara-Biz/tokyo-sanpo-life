89d7c032187c6c3ecbbf195d039e2bda258d9bd1
Liberty Takehara <libertytakehara@Libertys-MacBook-Air.local>
2024-01-04 02:08:09 +0900

    ci

---
+++ run-before-deploy.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# this script is used when running on local, and github actions to deploy.
+
+set -x
+
+echo "(1/5) prepare env related files"
+./env/overwriteEnvJSFiles.bash $1
+
+echo "(2/5) transpile typescript"
+tsc ./public/javascripts/*.ts
+
+echo "(3/5) set cache buster for css files"
+# Cache Buster
+# reference https://zenn.dev/manase/scraps/fca1f8c03fc4d7
+datetime=$(date +"%Y%m%d%H%M%S")
+find "functions/src/views" -type f -name "*.ejs" -exec sed -i '' "s/ver=[0-9]\{14\}/ver=$datetime/g" {} \;
+
+echo "(4/5) build tailwindcss file"
+cd functions
+npx tailwindcss -i ../public/stylesheets/tw-src.css -o ../public/stylesheets/tw-dest.css
+
+echo "(5/5) npm"
+npm install
+npm run build
+
+set +x
--- run-on-local.bash
+++ run-on-local.bash
@@ -1,25 +1,4 @@
 #!/bin/bash
 
-set -x
-
-echo "(1/5) prepare env related files"
-./env/overwriteEnvJSFiles.bash local
-
-echo "(2/5) transpile typescript"
-tsc ./public/javascripts/*.ts
-
-echo "(3/5) set cache buster for css files"
-# Cache Buster
-# reference https://zenn.dev/manase/scraps/fca1f8c03fc4d7
-datetime=$(date +"%Y%m%d%H%M%S")
-find "functions/src/views" -type f -name "*.ejs" -exec sed -i '' "s/ver=[0-9]\{14\}/ver=$datetime/g" {} \;
-
-echo "(4/5) build tailwindcss file"
-cd functions
-npx tailwindcss -i ../public/stylesheets/tw-src.css -o ../public/stylesheets/tw-dest.css
-
-echo "(5/5) execute!"
-npm run build
+./run-before-deploy.bash local
 firebase serve
-
-set +x
