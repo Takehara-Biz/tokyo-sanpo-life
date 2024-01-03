02015c90b9fe837d36a68309b839892c0d8514e7
Liberty Takehara <libertytakehara@Libertys-MacBook-Air.local>
2024-01-04 02:26:39 +0900

    ci

--- run-before-deploy.bash
+++ run-before-deploy.bash
@@ -4,6 +4,8 @@
 
 set -x
 
+npm install
+
 echo "(1/5) prepare env related files"
 ./env/overwriteEnvJSFiles.bash $1
 
@@ -11,17 +13,13 @@ echo "(2/5) transpile typescript"
 tsc ./public/javascripts/*.ts
 
 echo "(3/5) set cache buster for css files"
-# Cache Buster
-# reference https://zenn.dev/manase/scraps/fca1f8c03fc4d7
-datetime=$(date +"%Y%m%d%H%M%S")
-find "functions/src/views" -type f -name "*.ejs" -exec sed -i '' "s/ver=[0-9]\{14\}/ver=$datetime/g" {} \;
+./run-cache-buster.bash
 
 echo "(4/5) build tailwindcss file"
 cd functions
 npx tailwindcss -i ../public/stylesheets/tw-src.css -o ../public/stylesheets/tw-dest.css
 
 echo "(5/5) npm"
-npm install
 npm run build
 
 set +x
---
+++ run-cache-buster.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+# Reference
+# https://stackoverflow.com/questions/43171648/sed-gives-sed-cant-read-no-such-file-or-directory
+SEDOPTION=
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SEDOPTION="-i ''"
+fi
+
+# Cache Buster
+# reference https://zenn.dev/manase/scraps/fca1f8c03fc4d7
+datetime=$(date +"%Y%m%d%H%M%S")
+find "functions/src/views" -type f -name "*.ejs" -exec sed $SEDOPTION "s/ver=[0-9]\{14\}/ver=$datetime/g" {} \;
+
+set +x
